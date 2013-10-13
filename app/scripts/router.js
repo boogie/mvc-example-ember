4c460d058b6947c4710c85c878eb76bb042a1c75
Andras Barthazi <andras@barthazi.hu>
2013-10-13 14:25:54 +0200

    Playing with a User Card popup

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,6 +2,7 @@ EmberApp.router = Ember.Router.map(function () {
 	this.resource('home');
 	this.resource('user', function(){
 		this.route('search', { queryParams: ['query'] });
+		this.route('card', { path: 'card/:id' });
 	});
 });
 
